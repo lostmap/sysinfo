c1a6c4d67d45af911ba55e47872996ac1b201ed6
lostmap <lostmap4@gmail.com>
2016-12-20 12:24:44 +0400

    Update addsudousers.sh

--- addsudousers.sh
+++ addsudousers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash  
 while [[ -n $1 ]]; do 
-     echo "$1	ALL=(ALL) NOPASSWD: /usr/sbin/tcpdump" >> /etc/sudoers; 
-     echo "$1	ALL=(ALL) NOPASSWD: /bin/netstat" >> /etc/sudoers; 
+     echo "$1	ALL=(ALL) NOPASSWD: /usr/sbin/tcpdump" >> /etc/sudoers;  
     shift # shift all parameters; 
 done
